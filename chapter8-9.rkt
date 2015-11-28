93a52c4c068c9d02a52843f2e1bc1756cdab3e5a
David Grenier <davidgrenier@gmail.com>
2015-11-27 20:19:25 -0500

    Merges chapter 8 & 9, redid chapter 8

--- chapter8-9.rkt
+++ chapter8-9.rkt
@@ -26,16 +26,11 @@
                              'nil
                              (set? (cdr xs)))))
                    (size xs)
+                   ((A E E) (size/cdr xs))
+                   ((A E) (if-same (member? (car xs) (cdr xs)) 't))
+                   ((A) (if-same (atom xs) 't))
                    ((Q) (natp/size xs))
-                   (() (if-true (if (atom xs)
-                                    't
-                                    (if (member? (car xs) (cdr xs))
-                                        't
-                                        (< (size (cdr xs)) (size xs))))
-                                'nil))
-                   ((E E) (size/cdr xs))
-                   ((E) (if-same (member? (car xs) (cdr xs)) 't))
-                   (() (if-same (atom xs) 't))))))
+                   (() (if-true 't 'nil))))))
 
 (defun defun.add-atoms ()
   (J-Bob/define (defun.set?)
@@ -47,15 +42,10 @@
                          (add-atoms (car x)
                                     (add-atoms (cdr x) ys))))
                    (size x)
+                   ((A E Q) (size/car x))
+                   ((A E A) (size/cdr x))
+                   ((A E) (if-true 't 'nil))
+                   ((A) (if-same (atom x) 't))
                    ((Q) (natp/size x))
-                   (() (if-true (if (atom x)
-                                    't
-                                    (if (< (size (car x)) (size x))
-                                        (< (size (cdr x)) (size x))
-                                        'nil))
-                                'nil))
-                   ((E A) (size/cdr x))
-                   ((E Q) (size/car x))
-                   ((E) (if-true 't 'nil))
-                   (() (if-same (atom x) 't))))))
+                   (() (if-true 't 'nil))))))
 
